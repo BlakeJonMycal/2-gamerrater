e0a6b5891ed7f7d840fbcbb361f1bdd0d98d3171
Blake Jon Mycal Smith <B.jonmycal@gmail.com>
2024-08-28 15:26:27 -0400

    models created and migration

---
+++ raterapi/models/game_image.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class GameImage(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="image_created")
+    game = models.ForeignKey("Game", on_delete=models.CASCADE, related_name="game_image")
+    img = models.ImageField(upload_to='game_images/')
